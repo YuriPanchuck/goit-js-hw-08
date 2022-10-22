9f4f5e9d88d0cc4617e1550d1d797d7e6cb12557
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-10-23 00:18:02 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
+
+setCurrentTime();
+
+player.on('timeupdate', throttle(playedTime, 1000));
+
+function playedTime(data) {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+}
+
+function setCurrentTime() {
+  const savedTimeToPlay = localStorage.getItem('videoplayer-current-time');
+  if (savedTimeToPlay) {
+    player.setCurrentTime(savedTimeToPlay);
+  }
+}
 
